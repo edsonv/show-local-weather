51697e5ce9894beadce97fdf4d92173554c713af
Edson Vargas <edson.jhair@yahoo.com>
2018-03-28 23:37:17 -0400

    modifying js file

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 	if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function(position) {
     $("#location").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+    alert(latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
   });
 }
 
