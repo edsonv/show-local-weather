58a910fb28ad398dd6dae5104a8ec1a23f1c3336
Edson Vargas <edson.jhair@yahoo.com>
2018-03-28 23:36:15 -0400

    modifying js file

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
 
-	
+	if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    $("#location").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+  });
+}
 
 	var api = "https://api.openweathermap.org/data/2.5/weather?q=caracas&APPID=cecab3666714a2c3adb3b520d34f2beb";
 
